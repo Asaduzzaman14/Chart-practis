9d835c8b4729d132a95108bca61ff0643228f511
Md Asaduzaman <asadasad9282@gmail.com>
2022-03-29 00:54:14 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MyLineChart from './component/LineChart';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>hello</h2>
+      <MyLineChart></MyLineChart>
+    </div>
+  );
+}
+
+export default App;
